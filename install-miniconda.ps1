cbc7505080a1bcf680a2b0b5dcb3084f9484cee3
Alexander Pacha <alexander.pacha@gmail.com>
2017-10-18 15:48:52 +0200

    Moved windows requirements into separate file. Updated install-ps script to actually install all required packages

--- install-miniconda.ps1
+++ install-miniconda.ps1
@@ -32,6 +32,7 @@ if ($env:DEBUG) {
 $MINICONDA_URL = "https://repo.continuum.io/miniconda/"
 
 $env:USED_NUMPY_VERSION = "1.13"
+$env:PYTHON_VERSION = 2.7
 
 if (! $env:PIP_FALLBACK) {
    $env:PIP_FALLBACK = "True"
@@ -114,11 +115,6 @@ if (! $env:PYTHON) {
 InstallMiniconda $env:MINICONDA_VERSION $env:PLATFORM $env:PYTHON
 checkLastExitCode
 
-
-# Interrupt here for the moment
-exit
-
-
 # Set environment variables
 $env:PATH = "${env:PYTHON};${env:PYTHON}\Scripts;" + $env:PATH
 
@@ -172,141 +168,11 @@ $env:PATH = "${env:PYTHON}\envs\test;${env:PYTHON}\envs\test\Scripts;${env:PYTHO
 # Check that we have the expected version of Python
 python --version
 checkLastExitCode
-
-# CORE DEPENDENCIES
-# Remove pytest version limitation once
-# https://github.com/astropy/astropy/pull/6419 is solved
-Copy-Item ci-helpers\appveyor\pinned ${env:PYTHON}\envs\test\conda-meta\pinned
-
-conda install $QUIET -n test pytest pip
+pip --version
 checkLastExitCode
 
-# Check whether a specific version of Numpy is required
-if ($env:NUMPY_VERSION) {
-    if($env:NUMPY_VERSION -match "stable") {
-        $NUMPY_OPTION = "numpy=" + $env:LATEST_NUMPY_STABLE
-    } elseif($env:NUMPY_VERSION -match "dev") {
-        $NUMPY_OPTION = "Cython pip".Split(" ")
-    } else {
-        $NUMPY_OPTION = "numpy=" + $env:NUMPY_VERSION
-    }
-    conda install -n test $QUIET $NUMPY_OPTION
-    checkLastExitCode
-} else {
-    $NUMPY_OPTION = ""
-}
+conda install $QUIET -n test pytest pip numpy scipy matplotlib pytest h5py scikit-image
+checkLastExitCode
 
-# Check whether a specific version of Astropy is required
-if ($env:ASTROPY_VERSION) {
-    if($env:ASTROPY_VERSION -match "stable") {
-        $ASTROPY_OPTION = "astropy=" + $env:LATEST_ASTROPY_STABLE
-    } elseif($env:ASTROPY_VERSION -match "dev") {
-        $ASTROPY_OPTION = "Cython pip jinja2".Split(" ")
-    } elseif($env:ASTROPY_VERSION -match "lts") {
-        $ASTROPY_OPTION = "astropy=" + $env:ASTROPY_LTS_VERSION
-    } else {
-        $ASTROPY_OPTION = "astropy=" + $env:ASTROPY_VERSION
-    }
-    $output = cmd /c conda install -n test $QUIET $NUMPY_OPTION $ASTROPY_OPTION 2>&1
-    echo $output
-    if (($output | select-string UnsatisfiableError) -and $env:PIP_FALLBACK) {
-       echo "Installing astropy with conda was unsuccessful, using pip instead"
-       pip install $ASTROPY_OPTION
-       checkLastExitCode
-    } else {
-      checkLastExitCode
-    }
-} else {
-    $ASTROPY_OPTION = ""
-}
-
-# Check whether a specific version of Sunpy is required
-if ($env:SUNPY_VERSION) {
-    if($env:SUNPY_VERSION -match "stable") {
-        $SUNPY_OPTION = "sunpy"
-    } elseif($env:SUNPY_VERSION -match "dev") {
-        $SUNPY_OPTION = ""
-    } else {
-        $SUNPY_OPTION = "sunpy=" + $env:SUNPY_VERSION
-    }
-    $output = cmd /c conda install -n test $QUIET $NUMPY_OPTION $SUNPY_OPTION 2>&1
-    echo $output
-    if (($output | select-string UnsatisfiableError) -and $env:PIP_FALLBACK) {
-       echo "Installing sunpy with conda was unsuccessful, using pip instead"
-       pip install $SUNPY_OPTION
-       checkLastExitCode
-    } else {
-      checkLastExitCode
-    }
-} else {
-    $SUNPY_OPTION = ""
-}
-
-# Install the specified versions of numpy and other dependencies
-if ($env:CONDA_DEPENDENCIES) {
-    $CONDA_DEPENDENCIES = $env:CONDA_DEPENDENCIES.split(" ")
-} else {
-    $CONDA_DEPENDENCIES = ""
-}
-
-# If NUMPY_OPTION and CONDA_DEPENDENCIES are both empty, we skip this step
-if ($NUMPY_OPTION -or $CONDA_DEPENDENCIES) {
-
-  $output = cmd /c conda install -n test $QUIET $NUMPY_OPTION $CONDA_DEPENDENCIES 2>&1
-  echo $output
-  if (($output | select-string UnsatisfiableError, PackageNotFoundError) -and $env:PIP_FALLBACK) {
-     echo "Installing dependencies with conda was unsuccessful, using pip instead"
-     $output = cmd /c pip install $CONDA_DEPENDENCIES 2>&1
-     echo $output
-     checkLastExitCode
-     if ($output | select-string UnsatisfiableError, PackageNotFoundError) {
-        Exit 1
-     }
-  } else {
-    checkLastExitCode
-  }
-
-}
-
-# Check whether the developer version of Numpy is required and if yes install it
-if ($env:NUMPY_VERSION -match "dev") {
-   Invoke-Expression "${env:CMD_IN_ENV} pip install git+https://github.com/numpy/numpy.git#egg=numpy --upgrade --no-deps"
-   checkLastExitCode
-}
-
-# Check whether the developer version of Astropy is required and if yes install
-# it. We need to include --no-deps to make sure that Numpy doesn't get upgraded.
-if ($env:ASTROPY_VERSION -match "dev") {
-   Invoke-Expression "${env:CMD_IN_ENV} pip install git+https://github.com/astropy/astropy.git#egg=astropy --upgrade --no-deps"
-   checkLastExitCode
-}
-
-# Check whether the developer version of Sunpy is required and if yes install
-# it. We need to include --no-deps to make sure that Numpy doesn't get upgraded.
-if ($env:SUNPY_VERSION -match "dev") {
-   Invoke-Expression "${env:CMD_IN_ENV} pip install git+https://github.com/sunpy/sunpy.git#egg=sunpy --upgrade --no-deps"
-   checkLastExitCode
-}
-
-# We finally install the dependencies listed in PIP_DEPENDENCIES. We do this
-# after installing the Numpy versions of Numpy or Astropy. If we didn't do this,
-# then calling pip earlier could result in the stable version of astropy getting
-# installed, and then overritten later by the dev version (which would waste
-# build time)
-
-if ($env:PIP_FLAGS) {
-    $PIP_FLAGS = $env:PIP_FLAGS.split(" ")
-} else {
-    $PIP_FLAGS = ""
-}
-
-if ($env:PIP_DEPENDENCIES) {
-    $PIP_DEPENDENCIES = $env:PIP_DEPENDENCIES.split(" ")
-} else {
-    $PIP_DEPENDENCIES = ""
-}
-
-if ($env:PIP_DEPENDENCIES) {
-    pip install $PIP_DEPENDENCIES $PIP_FLAGS
-    checkLastExitCode
-}
+pip install -r requirements.txt
+pip install -r requirements_windows.txt
