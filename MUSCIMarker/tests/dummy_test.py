aee155ce2c948fee1e65a4642be2876f811a0be6
Alexander Pacha <alexander.pacha@gmail.com>
2018-10-30 10:41:05 +0100

    Refactored dummy-test into a skeleton for future tests

--- MUSCIMarker/tests/dummy_test.py
+++ MUSCIMarker/tests/dummy_test.py
@@ -1,6 +1,12 @@
+import unittest
+
 def inc(x):
     return x + 1
 
+class DummyTest(unittest.TestCase):
+    def test_simple_increment(self):
+        assert inc(4) == 5
 
-def test_answer():
-    assert inc(4) == 5
+
+if __name__ == '__main__':
+    unittest.main()
